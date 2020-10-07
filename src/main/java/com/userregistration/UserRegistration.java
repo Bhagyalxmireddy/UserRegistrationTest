8db159da6bcac758c0451701c7fe4fdfcb5bef4b
Bhagi <puppyreddy1997@gmail.com>
2020-10-07 09:12:32 +0530

    [Bhagyalaxmi]Added:validating the emailaddress

--- src/main/java/com/userregistration/UserRegistration.java
+++ src/main/java/com/userregistration/UserRegistration.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 public class UserRegistration {
 
     public static final String NAME_PATTERN = "^[A-Z]{1}[a-zA-Z]{2,}$";
-
+    public static final String EMAIL_PATTERN = "^[a-zA-Z0-9]+(.[a-zA-Z0-9]+)*@[a-zA-Z]+.[a-zA-Z]{2,4}(.[a-zA-Z]{2,2})?$";
     public boolean registrationFirstName(String fname) {
         Pattern pattern = Pattern.compile(NAME_PATTERN);
         return pattern.matcher(fname).matches();
@@ -14,5 +14,10 @@ public class UserRegistration {
     public boolean registrationLastName(String lname) {
         Pattern pattern = Pattern.compile(NAME_PATTERN);
         return pattern.matcher(lname).matches();
+    }
+
+    public boolean registrationEmailAddress(String email) {
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        return pattern.matcher(email).matches();
     }
 }
--- src/test/java/com/userregistration/UserRegistrationTest.java
+++ src/test/java/com/userregistration/UserRegistrationTest.java
@@ -5,36 +5,35 @@ import org.junit.Test;
 
 public class UserRegistrationTest {
     @Test
-    public void givenFirstName_WhenProper_ShouldReturnTrue()
-    {
+    public void givenFirstName_WhenProper_ShouldReturnTrue() {
         UserRegistration registration = new UserRegistration();
         boolean result = registration.registrationFirstName("Bhagi");
         Assert.assertTrue(result);
     }
+
     @Test
-    public void givenFirstName_WhenHaveSplChar_ShouldReturnFalse()
-    {
+    public void givenFirstName_WhenHaveSplChar_ShouldReturnFalse() {
         UserRegistration registration = new UserRegistration();
         boolean result = registration.registrationFirstName("Bh@gi");
         Assert.assertFalse(result);
     }
+
     @Test
-    public void givenFirstName_WhenHaveSplCharAndNum_ShouldReturnFalse()
-    {
+    public void givenFirstName_WhenHaveSplCharAndNum_ShouldReturnFalse() {
         UserRegistration registration = new UserRegistration();
         boolean result = registration.registrationFirstName("Bh@gi123");
         Assert.assertFalse(result);
     }
+
     @Test
-    public void givenFirstName_WhenHaveNum_ShouldReturnFalse()
-    {
+    public void givenFirstName_WhenHaveNum_ShouldReturnFalse() {
         UserRegistration registration = new UserRegistration();
         boolean result = registration.registrationFirstName("Bhagi11");
         Assert.assertFalse(result);
     }
+
     @Test
-    public void givenFirstName_WhenNotProper_ShouldReturnFalse()
-    {
+    public void givenFirstName_WhenNotProper_ShouldReturnFalse() {
         UserRegistration registration = new UserRegistration();
         boolean result = registration.registrationFirstName("bhagi");
         Assert.assertFalse(result);
@@ -46,12 +45,14 @@ public class UserRegistrationTest {
         boolean result = registration.registrationLastName("Bhagi");
         Assert.assertTrue(result);
     }
+
     @Test
     public void givenLastName_WhenHaveSplChar_ShouldReturnFalse() {
         UserRegistration registration = new UserRegistration();
         boolean result = registration.registrationLastName("Bh@gi");
         Assert.assertFalse(result);
     }
+
     @Test
     public void givenLastName_WhenHaveNum_ShouldReturnFalse() {
         UserRegistration registration = new UserRegistration();
@@ -65,10 +66,30 @@ public class UserRegistrationTest {
         boolean result = registration.registrationLastName("Bh@gi123");
         Assert.assertFalse(result);
     }
+
     @Test
     public void givenLastName_WhenNotProper_ShouldReturnFalse() {
         UserRegistration registration = new UserRegistration();
         boolean result = registration.registrationLastName("bhagi");
         Assert.assertFalse(result);
     }
+
+    @Test
+    public void givenEmailAddress_WhenProper_ShouldReturnTure() {
+        UserRegistration registration = new UserRegistration();
+        boolean result = registration.registrationEmailAddress("abc@gmail.com");
+        Assert.assertTrue(result);
     }
+    @Test
+    public void givenEmailAddress_WhenWithContiuneSplChar_ShouldReturnFalse() {
+        UserRegistration registration = new UserRegistration();
+        boolean result = registration.registrationEmailAddress("abc@.gmail.com");
+        Assert.assertFalse(result);
+    }
+    @Test
+    public void givenEmailAddress_WhenNotProper_ShouldReturnFalse() {
+        UserRegistration registration = new UserRegistration();
+        boolean result = registration.registrationEmailAddress("abc@gmail.in.");
+        Assert.assertFalse(result);
+    }
+}
